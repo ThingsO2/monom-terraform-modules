0f03b5e5f277253beefe5bb06d0fbc9d291a667d
César M. Cristóbal <cmartin@monom.ai>
2021-12-14 09:46:11 +0100

    Create grafana users with name and login get from mongo

---
+++ grafana-user-org/get_mongo_login_users.py
@@ -0,0 +1,17 @@
+from json import dumps as json_dumps
+import sys
+import get_mongo_users
+
+if __name__ == "__main__":
+    # get arguments
+    mongo_connection_string = sys.argv[1]
+    project = sys.argv[2]
+    # get users
+    users = get_mongo_users.get_users(mongo_connection_string=mongo_connection_string, project=project)
+    # generate json with logins
+    logins = []
+    for user in users:
+        logins.append(user['_id']['$oid'])
+    # print json
+    out = dict(logins=','.join(logins))
+    print(json_dumps(out))
